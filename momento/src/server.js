1737d169b20c584a66a3b285212f52651db6d2a7
Cristhian Loya <15186@ulsachihuahua.edu.mx>
2026-06-04 05:05:53 -0600

    feat: configurar Apollo Server con contexto JWT

--- momento/src/server.js
+++ momento/src/server.js
@@ -1,8 +1,26 @@
+require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
+const { verificarToken } = require('./auth');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        const token = req.headers.authorization || '';
+
+        if (token) {
+            try {
+                const usuario = verificarToken(token.replace('Bearer ', ''));
+                return { usuario };
+            } catch (error) {
+                return { usuario: null };
+            }
+        }
+        return { usuario: null };
+    }
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀 Servidor listo en ${url} 🚀`);
